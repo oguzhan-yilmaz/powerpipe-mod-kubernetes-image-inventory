40c3f645865ed41a8a4be274c68f14438a4c48f9
Oguzhan Yilmaz <oguzhan@hepapi.com>
2025-07-30 10:59:03 +0200

    Add query for retrieving pod container images from Kubernetes

---
+++ queries/pod_images.pp
@@ -0,0 +1,16 @@
+
+query "pod_container_images" {
+  sql = <<-EOQ
+    SELECT 
+        context_name,
+        namespace,
+        name as pod_name,
+        container_info->>'name' as container_name,
+        container_info->>'image' as image,
+        container_info->>'imagePullPolicy' as image_pull_policy
+    FROM kubernetes_pod,
+        jsonb_array_elements(containers::jsonb) as container_info;
+  EOQ
+}
+
+
